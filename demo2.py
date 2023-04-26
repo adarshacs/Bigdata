5f3dd211ee2c6d7026f4ea4cf4536fb692efbb1f
adarshacs <adarshacs1998@gmail.com>
2023-04-26 17:20:06 +0530

    alll file

--- demo2.py
+++ demo2.py
@@ -1,5 +1,16 @@
-print("python files start");
+print("python files start")
 # print("python file ended")
+
+
+def run(name):
+    print(name)
+
+
+x = 5
+while x < 10:
+    x += 1
+    print(x)
 
 print("add 16:11")
 print("today 25-04-2023")
+run("addd")
---
+++ demo3.py
@@ -0,0 +1,5 @@
+print("jjj")
+st = input('Enter X and Y')
+
+X, Y = [int(x) for x in st.split(',')]
+
