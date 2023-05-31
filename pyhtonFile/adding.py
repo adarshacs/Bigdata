aae84c3c8ff6fef630a4737c337b5e4fb1a3bae1
adarshacs <adarshacs1998@gmail.com>
2023-05-31 17:06:43 +0530

    branch1 remove 5th line add 7th line

--- pyhtonFile/adding.py
+++ pyhtonFile/adding.py
@@ -2,5 +2,6 @@ print("adding")
 print ("adding 2nd line")
 print ("adding 3rd line")
 print ("adding 4th line")
-print ("adding 5th line")
+
 print ("adding 6th line")
+print ("remove 5th add 7th line")
